0bc2ab87f7e7217bca30aaeb4ce2af55109bff75
guimamura <guilherme.imamura.fernandes@gmail.com>
2025-02-25 14:17:08 -0300

    feat: created grid system

---
+++ src/components/Col.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :class="colClass">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  cols: {
+    type: Number,
+    default: 12,
+  },
+})
+
+const colClass = computed(() => `col-${props.cols}`)
+</script>
+
+<style scoped>
+.col-1 {
+  flex-basis: calc(100% / 12 * 1);
+  max-width: calc(100% / 12 * 1);
+}
+.col-2 {
+  flex-basis: calc(100% / 12 * 2);
+  max-width: calc(100% / 12 * 2);
+}
+.col-3 {
+  flex-basis: calc(100% / 12 * 3);
+  max-width: calc(100% / 12 * 3);
+}
+.col-4 {
+  flex-basis: calc(100% / 12 * 4);
+  max-width: calc(100% / 12 * 4);
+}
+.col-6 {
+  flex-basis: calc(100% / 12 * 6);
+  max-width: calc(100% / 12 * 6);
+}
+.col-12 {
+  flex-basis: 100%;
+  max-width: 100%;
+}
+
+@media (max-width: 768px) {
+  [class^='col-'] {
+    flex-basis: 100%;
+    max-width: 100%;
+  }
+}
+</style>
---
+++ src/components/Row.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  gap: var(--row-gap, 16px);
+}
+</style>
